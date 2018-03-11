935b87b329b16c501f9d473f0a1ac15c47e42f84
Paweł Kapalla <pkapalla@xesenix.pl>
2018-03-11 20:23:50 +0100

    fix(webpack): enable working on linked modules
    when working with linked modules executing tests from module that requires some other core module that uses rewiremock can result in paths for process.cwd that link to completely different place on hard drive so we cant use it for evaluating relative paths instead we search for closest node_module on current file __dirname this way we actual get correct relative path to interceptor

--- webpack/plugin.js
+++ webpack/plugin.js
@@ -1,7 +1,13 @@
 const {relative} = require("path");
 const {ConcatSource} = require("webpack-sources");
 
-const file = './' + relative(process.cwd(), __dirname + '/interceptor.js').replace(/\\/g, '/');
+// when working with linked modules executing tests from module that requires some other core module that uses 
+// rewiremock can result in paths for process.cwd that link to completely different place on hard drive so we cant 
+// use it for evaluating relative paths instead we search for closest node_module on current file __dirname this 
+// way we actual get correct relative path to interceptor
+const nodeModulesIndex = __dirname.lastIndexOf('node_modules');
+const nodeModulesPath = __dirname.substr(0, nodeModulesIndex);
+const file = './' + relative(nodeModulesPath, __dirname + '/interceptor.js').replace(/\\/g, '/');
 
 const injectString = "/***/if(typeof __webpack_require__!=='undefined'){__webpack_require__ = __webpack_require__('" + file + "')(__webpack_require__, module);}\n";
 
